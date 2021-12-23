e941b27189875481354606142cf3e25cd88d2ef5
harpsaini <harpreetsinghsn@gmail.com>
2021-12-23 08:02:53 -0500

    enabled cors

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const app = express();
-
 const fs = require('fs')
+var cors = require('cors')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
   fs.readFile('./db.json', (err,data)=>{
